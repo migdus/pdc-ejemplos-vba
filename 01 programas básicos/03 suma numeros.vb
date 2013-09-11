3ea50aa004c40707946a3125d6b2d8dbb736b58d
Miguel Dussan <migueldussan@gmail.com>
2013-09-11 10:58:52 -0500

    Uso de Val() y de & para simplificar el uso del lenguaje.

--- 01 programas básicos/03 suma numeros.vb
+++ 01 programas básicos/03 suma numeros.vb
@@ -9,38 +9,33 @@ Sub Main() ' Inicia función principal
 Dim a As Integer ' Declaración de entero (primer número)
 Dim b As Integer ' Declaración de entero (segundo número)
 Dim c As Integer ' Declaración de entero (almacenar el resultado)
-Dim cadena_temp As String ' Declaración de cadena temporal, se utiliza para
-													' guardar los datos capturados por teclado, y luego
-													' convertirlos al tipo de dato Integer
-													
+
 a = 0 ' Inicialización de entero
 b = 0 ' Inicialización de entero
 c = 0 ' Inicialización de entero
-cadena_temp = "" ' Inicialización de cadena con una cadena vacía
 
-'Solicita el primer número y lo asigna inicialmente en la variable
-'cadena_temp, y luego se convierte a Integer y se guarda en a
-'esto se hace con ayuda de la función CInt() que toma como parámetro
-' una cadena
-cadena_temp = InputBox("Ingrese el primer número")
-a = CInt(cadena_temp)
+' Solicita el primer número y lo asigna a la variable a
+' La función InputBox("cadena") muestra un mensaje en pantalla con el texto
+' cadena, y le permite al usuario ingresar un valor.
+' La función Val(.) convierte lo que está como argumento a su respectivo
+' valor numérico. Se utiliza junto a InputBox como se muestra en la línea.
 
-'Solicita el segundo número y lo asigna a la variable b, de la misma
-'forma como se hizo con a
-cadena_temp = InputBox("Ingrese el segundo número")
-b = CInt(cadena_temp)
+a = Val(InputBox("Ingrese el primer número"))
+
+'Solicita el segundo número y lo asigna a la variable b
+
+b = Val(InputBox("Ingrese el segundo número"))
 
 c = a + b ' Realiza la operación y la asigna a c
 
 ' Variable el resultado de la operación
 Dim mensaje_resultado As String
 
-' Las variables numéricas deben convertirse a cadenas
-' Para eso se usa la función CStr() que recibe como parámetro
-' La variable con el valor numérico, y lo convierte en cadena.
-' Las cadenas se concatenan (unen) utilizando el operador "+".
+' Las variables numéricas se concatenan (se "juntan")
+' utilizando el caracter ampersand (&) y esta información
+' se almacena en una cadena que se mostrará al final
 
-mensaje_resultado = CStr(a) + " + " + CStr(b) + " = " + CStr(c)
+mensaje_resultado = a & " + " & b & " = " & c
 
 'Se muestra el resultado
 MsgBox (mensaje_resultado)
